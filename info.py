396101acab8035544c1924068cd21fc5279b74af
pawanjaatpk <118508835+pawanjaatpk@users.noreply.github.com>
2023-07-27 09:46:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,10 +21,10 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "6019386824:AAH2qeX_AckDCcdm3h-_0GCi865xUKi
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://telegra.ph/Welcome-pic-07-27 https://telegra.ph/Welcome-pic-2-07-27 https://telegra.ph/Welcome-pic-3-07-27 https://telegra.ph/Welcome-pic-07-27')).split()
-NOR_IMG = environ.get("NOR_IMG", "https://te.legra.ph/file/a27dc8fe434e6b846b0f8.jpg")
+PICS = (environ.get('PICS', 'https://telegra.ph/Welcome-pic-5-07-27 https://telegra.ph/Welcome-pic-4-07-27 https://telegra.ph/Welcome-pic-07-27 https://telegra.ph/Welcome-pic-5-07-27')).split()
+NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/Welcome-pic-5-07-27")
 MELCOW_VID = environ.get("MELCOW_VID", "https://te.legra.ph/file/6f55d902f9bf2d0afd4bb.mp4")
-SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
+SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/Spelling-check-07-27")
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5772711610').split()]
